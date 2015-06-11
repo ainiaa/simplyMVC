d82dd4e103a9778f793a9bb0f4699390cb9282cc
ainiaa <liuwenyuan521888@163.com>
2015-06-11 17:24:27 +0800

    1、修改配置项
    1、修改配置项

--- config/storage.split.inc.php
+++ config/storage.split.inc.php
@@ -3,7 +3,7 @@
  * db 分表分库配置
  */
 return array(
-        'db'    => array(//db 分表
+        'dbSplit'    => array(//db 分表
                 'table'  => array(//分表相关配置
                         'tableName' => array(
                                 'sliceCount' => '10',//切片数量 (分成10张表)
@@ -15,11 +15,11 @@ return array(
                         'slicePK'    => 'uid',//切片依据(根据uid散列)
                 ),
         ),
-        'mc'    => array(//mc 分布
+        'mcSplit'    => array(//mc 分布
                 'sliceCount' => '3',//切片数量 (分成3个部分)
                 'slicePK'    => 'uid',//切片依据(根据uid散列)
         ),
-        'redis' => array(
+        'redisSplit' => array(
                 'sliceCount' => '3',//切片数量 (分成3个部分)
                 'slicePK'    => 'uid',//切片依据(根据uid散列)
         ),
