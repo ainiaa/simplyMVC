b653dfde730b7bd71a3a851a84652af4e2adbc81
ainiaa <liuwenyuan521888@163.com>
2015-07-31 15:13:12 +0800

    1、修改DebugHelper
    1、修改DebugHelper

--- core/helper/SmvcDebugHelper.class.php
+++ core/helper/SmvcDebugHelper.class.php
@@ -71,11 +71,17 @@ class SmvcDebugHelper implements ArrayAccess
 
     /**
      * @static
-     * @return SmvcDebugHelper
+     *
+     * @param string $instanceName
+     *
+     * @return DebugHelper
      */
-    public static function getInstance()
+    public static function getInstance($instanceName = 'firephp')
     {
-        if (self::$instance == null) {
+        $id = md5($instanceName);
+        if (isset(self::$instance[$id]) && self::$instance[$id] instanceof self) {
+
+        } else {
             $instance = new SmvcDebugHelper();
             $bool     = false;
             if (C('smvcDebug')) {
@@ -85,13 +91,17 @@ class SmvcDebugHelper implements ArrayAccess
             $instance->_isdebug = $bool;
 
             if ($instance->_isdebug) {
-                $instance->debugInstance = FirePHP::getInstance(true);
-                $instance->debugInstance->setEnabled($bool);
-            }
-            self::$instance = $instance;
-        }
+                if ($instanceName === 'firephp') {
+                    $instance->debugInstance = FirePHP::getInstance(true);
+                    $instance->debugInstance->setEnabled($bool);
+                } else {
+                    $instance->debugInstance = new $instanceName;
+                }
 
-        return self::$instance;
+            }
+            self::$instance[$id] = $instance;
+        }
+        return self::$instance[$id];
     }
 
     /**
@@ -112,30 +122,110 @@ class SmvcDebugHelper implements ArrayAccess
         if (!in_array($debugLevel, array('log', 'info', 'error', 'warn'), true)) {
             $debugLevel = 'log';
         }
-
         return $this->debugInstance->$debugLevel($info, $label, $options);
     }
 
     /**
      * @author Jeff Liu
      *
-     * @param string $info
+     * @param mixed  $info
      * @param string $label
-     * @param string $debugLevel
-     * @param array  $options
+     * @param string $level
+     * @param string $options
      *
      * @return bool
      */
-    public function debug($info = '', $label = '', $debugLevel = '', $options = array())
+    public function debug($info = '', $label = '', $level = '', $options = '')
     {
         if (!$this->_isdebug) {
             return false;
         }
-        $debugLevel = strtolower($debugLevel); //默认应该是log
+        $debugLevel = strtolower($level); //默认应该是log
         if (!in_array($debugLevel, array('log', 'info', 'error', 'warn'), true)) {
             $debugLevel = 'log';
         }
-
         return $this->debugInstance->$debugLevel($info, $label, $options);
     }
+
+    /**
+     * @author Jeff Liu<liuwy@imageco.com.cn>
+     *
+     * @param mixed  $info
+     * @param string $label
+     * @param string $level
+     * @param mixed  $options
+     *
+     * @return bool
+     */
+    public function fileDebug($info = '', $label = '', $level = '', $options = '', $debugFile = '')
+    {
+        if (!$this->_isdebug) {
+            return false;
+        }
+        $msgFormat = '%s|%s|%s|%s|%s' . PHP_EOL;
+
+        if (empty($debugFile)) {
+            if (defined('IS_WIN') && IS_WIN) {
+                $defaultFile = 'c:/Windows/Temp/fileDebug';
+            } else {
+                $defaultFile = '/tmp/fileDebug';
+            }
+            $debugFile = C('DEBUG_FILE', $defaultFile);
+        } else {
+            $parentDir = dirname($debugFile);
+            if (!file_exists($parentDir)) {
+                $mkdirResult = mkdir($parentDir, 777, true);
+                if ($mkdirResult === false) {//文件夹创建失败
+                    trigger_error($parentDir.'create failure!',E_USER_WARNING);
+                }
+            }
+        }
+
+        $msg = $this->formatMsg($info, $label, $level, $options, $msgFormat);
+        error_log($msg, 3, $debugFile);
+        return true;
+    }
+
+    /**
+     * 格式化消息
+     * @author Jeff Liu<liuwy@imageco.com.cn>
+     *
+     * @param mixed  $info
+     * @param string $label
+     * @param string $level
+     * @param mixed  $options
+     * @param string $msgFormat
+     *
+     * @return string
+     */
+    private function formatMsg($info = '', $label = '', $level = '', $options = '', $msgFormat = '')
+    {
+        if (empty($msgFormat)) {
+            $msgFormat = '%s|%s|%s|%s|%s' . PHP_EOL;
+        }
+        return sprintf($msgFormat, date('Y-m-d H:i:s'), $level, $label, var_export($info, 1), var_export($options, 1));
+    }
+
+    /**
+     * todo
+     *
+     * @param mixed  $info
+     * @param string $label
+     * @param string $level
+     * @param mixed  $options
+     *
+     * @return bool
+     */
+    public function socketDebug($info = '', $label = '', $level = '', $options = '')
+    {
+        if (!$this->_isdebug) {
+            return false;
+        }
+        $msgFormat = '%s|%s|%s|%s|%s' . PHP_EOL;
+
+        $msg = $this->formatMsg($info, $label, $level, $options, $msgFormat);
+        //todo 将msg写入指定socket
+        return true;
+    }
+
 }
