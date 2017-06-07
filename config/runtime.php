ed8b7951531e47c3e9f810014df713a6791b0400
ainiaa <liuwenyuan521888@163.com>
2017-06-07 09:04:29 +0800

    1、添加新的autoLoadPath
    1、添加新的autoLoadPath

--- config/runtime.php
+++ config/runtime.php
@@ -15,7 +15,7 @@ return [
     //COOKIE 域名
         'cookieDomain'             => '',
     //类自动加载目录 多个目录之间使用逗号分割
-        'autoLoadPath'             => CORE_DIR . ',' . CORE_DIR . 'cache/' . ',' . CORE_DIR . 'i18n/' . ',' . CORE_DIR . 'helper/' . ',' . CORE_DIR . 'session/',
+        'autoLoadPath'             => CORE_DIR . ',' . CORE_DIR . 'cache/' . ',' . CORE_DIR . 'i18n/' . ',' . CORE_DIR . 'helper/' . ',' . CORE_DIR . 'session/'.','.INCLUDE_DIR.'vendor/',
         'defaultLocal'             => 'en_us',//默认语言
 
     //开启 路由过滤
@@ -27,17 +27,17 @@ return [
                 ['controller' => 'cc', 'action' => 'ca'],
         ],//需要 routerFilterMode 设置为 blacklist 可用
 
-        'DEFAULT_CACHE_TIME'  => 86400,//默认缓存时间
-        'SECOND_30'           => 30,//30秒
-        'MINUTE_1'            => 60,//1分钟
-        'MINUTE_5'            => 300,//5分钟
-        'MINUTE_30'           => 1800,//30分钟
-        'HOUR_1'              => 3600,//1小时
-        'HOUR_10'             => 50400,//10小时
-        'DAY_1'               => 86400,//1天
-        'WORKEND_1'           => 604800,//1周
-        'CURRENT_TIMESTAMP'   => $currentTime,//当前时间戳
-        'CURRENT_TIME_YMD'    => date('Y-m-d', $currentTime),//Y-m-d方式的时间
-        'CURRENT_TIME_YMDHIS' => date('Y-m-d H:i:s', $currentTime),//Y-m-d H:i:s方式的时间
-        'DEFAULT_URL'         => 'index.php',//默认访问页面
+        'DEFAULT_CACHE_TIME'       => 86400,//默认缓存时间
+        'SECOND_30'                => 30,//30秒
+        'MINUTE_1'                 => 60,//1分钟
+        'MINUTE_5'                 => 300,//5分钟
+        'MINUTE_30'                => 1800,//30分钟
+        'HOUR_1'                   => 3600,//1小时
+        'HOUR_10'                  => 50400,//10小时
+        'DAY_1'                    => 86400,//1天
+        'WORKEND_1'                => 604800,//1周
+        'CURRENT_TIMESTAMP'        => $currentTime,//当前时间戳
+        'CURRENT_TIME_YMD'         => date('Y-m-d', $currentTime),//Y-m-d方式的时间
+        'CURRENT_TIME_YMDHIS'      => date('Y-m-d H:i:s', $currentTime),//Y-m-d H:i:s方式的时间
+        'DEFAULT_URL' => 'index.php',//默认访问页面
 ];
